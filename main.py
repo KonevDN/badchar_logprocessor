3423eb556f4eb95ba56b34ed5d2ad5563d765ea6
Daniil <KonevDN@ya.ru>
2017-07-07 16:42:32 +0300

    Рабочая версия скрипта. - коммит перед тем, как я бду переделывать всю логику скрипта

--- main.py
+++ main.py
@@ -4,22 +4,28 @@ import parsing
 intStringSize = 2000
 strNameOfBadFile = 'log.log'
 
+
 if os.path.exists(strNameOfBadFile):
     print("File is exist")
 
     objNormalLogFile = open('normal.log', 'w')
     objBadLogFile = open(strNameOfBadFile, 'r')
 
-    arrayOfStrings = []
-
-    oneString = True
-    while oneString:
-        oneString = objBadLogFile.readline(intStringSize)  # считать файл по одной строке
-        arrayOfStrings = arrayOfStrings + [oneString]  # созадь массив под одной строке
-        strWithoutBadChar = parsing.deleteChar(oneString)  # очистить одну строку от плохих символов
+    arrayOfGoodStrings = []
+    strBadString = True
+    while strBadString:
+        strBadString = objBadLogFile.readline(intStringSize)  # считать файл по одной строке
+        strWithoutBadChar = parsing.deleteChar(strBadString)  # очистить одну строку от плохих символов
+        arrayOfGoodStrings.append(strWithoutBadChar.strip())  # созадь массив под одной строке
         print(strWithoutBadChar)
         objNormalLogFile.write(strWithoutBadChar+'\n')
+
+    #s = parsing.deleteEmptyElement(arrayOfGoodStrings)
+    #print(s)
+
     objBadLogFile.close()
     objNormalLogFile.close()
+
 else:
     print("File is No exist")
+
--- parsing.py
+++ parsing.py
@@ -1,31 +1,26 @@
-def deleteSlashChar( varString ):
-    array = list(varString)
-    # print(mass)
-    for index in range(len(array)):
-        # print(index)
-        if (array[index] == '\r') or (array[index] == '\n') or (array[index] == '>'):
-            array[index] = ' '
-    # print(mass)
 
-    resultString = ''.join(array)
-    # print(resultString)
-    return resultString
-
-
-def deleteChar(strbad:str):
+def deleteChar(strbad):
     strbad = strbad.replace('\\r', ' ')
     strbad = strbad.replace('\\n', ' ')
     strbad = strbad.replace('\r', ' ')
     strbad = strbad.replace('\n', ' ')
     strbad = strbad.replace('>', ' ')
     strbad = strbad.replace('<', ' ')
+    strbad = strbad.replace('\0', ' ')
+    #strbad = strbad.replace('-', ' ')
     strgood = strbad
-    return strgood
+    return str(strgood)
 
 
 
-
-
-# inputString = '18:13:54.155>  \r \n+CIPSTATUS: 0,0,"TCP","91.228.154.22","18000","CONNECTED" \r \n'
-# print(parsing.deleteSlashChar(inputString))
-# print(parsing.deleteSlashChar('10:18:30.011   \r \n+CPIN: NOT INSERTED \r \n')+'\n')
+def deleteEmptyElement(arrayOfString):
+    ''' функция получает массив и возвращает массив, она удаляет плохие элементы массива '''
+    arrayOfString = list(arrayOfString)
+    str1=''
+    str2=' '
+    arrayOfNewElements = []
+    for element in arrayOfString:
+        element
+        if element != str1 and element != str2:
+            arrayOfNewElements.append(element)
+    return arrayOfNewElements
