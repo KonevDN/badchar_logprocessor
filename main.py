bf9d4bb04aeafb303ef8e90a31198996aa93cf00
Daniil <KonevDN@ya.ru>
2017-07-07 21:32:09 +0300

    Рабочая версия скрипта. - первая вресия скрипта, который наконецтаки сделал весь лог читаемым! т.е. убраны плозие символы типа \n \r  и убраны бззпорядочные переносы строк - скрипт работает успешно, но есть проблемы: скрипт почему то удаляет некоторые лишние слова или строки, я еще не понял до конца, надо разбираться. но в целом лог теперь пригоден для чтения - дальше я уберу баги и сделаю расшифровку команд

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import os
+import re
 import parsing
 
 ''' глобальные переменные '''
 str_name_of_bad_file='log.log'
 str_name_of_good_file='normal.log'
 int_max_size_of_read_string=2000
+
 
 ''' работа '''
 array_from_file = parsing.get_array_from_file(str_name_of_bad_file, int_max_size_of_read_string)
@@ -16,6 +17,26 @@ print(array_of_good_strings)
 array_of_good_strings = parsing.delete_empty_element_from_array(array_of_good_strings)
 print(array_of_good_strings)
 
-parsing.put_array_to_file(array_of_good_strings, PRINT=True)
+
+mass = ['a','b','c','d']
+newmass=[]
+
+objTimeTemplate = re.compile(r'\d\d:\d\d:\d\d.\d\d\d')
+size = len(array_of_good_strings)
+for index in range(len(array_of_good_strings)):
+    if index<size-1:
+        match1 = objTimeTemplate.search(array_of_good_strings[index])
+        match2 = objTimeTemplate.search(array_of_good_strings[index+1])
+        if match1!=None and match2==None:
+            newmass.append('\n'+match1.group()+' '*5+array_of_good_strings[index+1])
+            s=newmass
+print(newmass)
+array_of_good_strings = newmass
+
+
+
+
+
+parsing.put_array_to_file(array_of_good_strings, PRINT=False)
 
 
--- parsing.py
+++ parsing.py
@@ -3,20 +3,23 @@ def get_array_from_file( str_name_of_bad_file='log.log', int_max_size_of_read_st
     int_max_size_of_read_string = int(int_max_size_of_read_string)  # int_max_size_of_read_string = 2000 для примера
     str_name_of_bad_file = str(str_name_of_bad_file)  # str_name_of_bad_file = 'log.log' для примера
 
+    array_from_file = []
     if os.path.exists(str_name_of_bad_file):
         print("File %s is exist" % str_name_of_bad_file)
 
         objBadLogFile = open(str_name_of_bad_file, 'r')
 
-        array_from_file = []
+
         str_one_bad_string = True
         while str_one_bad_string:
             str_one_bad_string = objBadLogFile.readline(int_max_size_of_read_string)  # считать файл по одной строке
             array_from_file.append(
                 str_one_bad_string.strip())  # созадь массив под одной строке и убрать пробелы по бокам и \n
         objBadLogFile.close()
+
     else:
         print("File is No exist")
+
     return array_from_file
 
 
@@ -50,17 +53,18 @@ def delete_empty_element_from_array( array_of_bad_strings ):
     str2 = ' '
     array_of_good_strings = []
     for str_one_element in array_of_bad_strings:
-        str_one_element
         if str_one_element != str1 and str_one_element != str2:
             array_of_good_strings.append(str_one_element)
     return array_of_good_strings
 
 
-def put_array_to_file(array_of_good_strings, str_name_of_good_file='normal.log', PRINT=True):
-    objNormalLogFile = open(str_name_of_good_file, 'w')
+def put_array_to_file( array_of_good_strings: list, str_name_of_good_file: str = 'normal.log',
+                       PRINT: bool = True ) -> None:
+    objNormalLogFile = open(str_name_of_good_file,'w')
     for str_one_element in array_of_good_strings:
-        str_one_element=str_one_element+'\n'
+        #str_one_element = str_one_element + '\n'
         objNormalLogFile.write(str_one_element)
-        if PRINT==True:
+        if PRINT == True:
             print(str_one_element)
     objNormalLogFile.close()
+    return None
