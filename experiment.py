6e3704d22f0b94fcb979333afaada8964b2f09b1
Daniil <KonevDN@ya.ru>
2017-07-21 19:59:11 +0300

    Рабочая версия скрипта: в эксперимантельном файле сделано много полезных улучшений - сделано автоматическое выравнивание количества смволов для номера строки в логе - сделан вывод даты времени в логе - сделано создание папки  дял логов - сделана запись самих файлов логов с правильными именами

--- experiment.py
+++ experiment.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+import os
 import time, datetime
 import serial.tools.list_ports
 
@@ -12,19 +13,35 @@ mass
 print(mass)
 '''
 
+''' Проверка и создание папки для логов'''
+if not os.path.exists('logs'):
+    os.makedirs('logs')
+
+''' Создание имени файла и самого файла  '''
+time.sleep(1)      # дополнительная простая защита от создания одинаковых имен файла, тк имя содержит "секунду"
+now = datetime.datetime.now()
+str_current_time = now.strftime("%Y-%m-%d__%Hh%Mm%Ss____")
+str_new_log_file_name = str_current_time +'FM3'+'.log'
+str_complete_new_log_dir_and_file_name = os.path.join('logs',str_new_log_file_name)
+objLogFile = open(str_complete_new_log_dir_and_file_name,'w')
+
+''' Вывод спика имеющихся COM-портов '''
 ports = serial.tools.list_ports.comports()
 for port in ports:
-    print(port)
+    print(str(port))
+    objLogFile.write(str(port)+'\r\n')  # .encode()
+    objLogFile.flush()
 
+''' Подключение к COM-порту и его настройка '''
 ser = serial.Serial()
 ser.port = 'COM4'
-ser.baudrate=57600
-ser.bytesize=8
-ser.parity='N'
-ser.stopbits=1
-# ser.timeout=1
-ser.xonxoff=0
-ser.rtscts=0
+ser.baudrate = 57600
+ser.bytesize = 8
+ser.parity = 'N'
+ser.stopbits = 1
+ser.timeout = 60
+ser.xonxoff = 0
+ser.rtscts = 0
 res = ser.open()
 
 int_number_of_line = 0
@@ -32,16 +49,19 @@ while 1:
     int_number_of_line += 1
     now = datetime.datetime.now()
     current_time = now.strftime("%Y-%m-%d\t%Hh%Mm%Ss")
-    #current_time = str(now.date()) + '\t' + str(now.hour) + 'h' + str(now.minute) + 'm' + str(now.second)+'s'
+    # current_time = str(now.date()) + '\t' + str(now.hour) + 'h' + str(now.minute) + 'm' + str(now.second)+'s'
     # line = str(int_number_of_line) +'\t'+ current_time +'\t\t'+ str(ser.readline().decode('utf-8').strip())
-    port_data = ser.readline()
-    #print("Bytes - `{0}`, string - `{1}`".format(repr(port_data), port_data.decode('utf-8')))
-    #port_data = port_data.decode('utf-8')
-    line = str(int_number_of_line) +'\t'+ current_time +'\t\t'+ str(port_data)#.decode('utf-8').strip()
-    print(line) # .encode('utf8')
-    #time.sleep(0.1)
+    port_data:bytes = ser.readline()
+    # port_data = ser.readline()
+    # print("Bytes - `{0}`, string - `{1}`".format(repr(port_data), port_data.decode('utf-8')))
+    port_data = port_data.decode()
+    line = str(int_number_of_line).ljust(6,' ') + '\t\t' + current_time + '\t\t' + str(port_data) # + '\r\n'  # .decode('utf-8').strip()
+    print(line,end='')  # .encode('utf8')
+    objLogFile.write(line) #.encode()
+    objLogFile.flush()
 
 print(ser.is_open)
 ser.close()
 print(ser.is_open)
-# False
+objLogFile.close()
+
